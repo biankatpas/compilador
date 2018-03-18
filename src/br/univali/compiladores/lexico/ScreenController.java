789ed8f87b7d02c90c8b045a1d87f63748a512c3
Bianka Passos <biankatpas@gmail.com>
2018-03-18 16:49:25 -0300

    screen controller

---
+++ src/br/univali/compiladores/lexico/ScreenController.java
@@ -0,0 +1,15 @@
+package br.univali.compiladores.lexico;
+
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author biankatpas
+ */
+public class ScreenController {
+    
+    public void newFile(JTextArea jta){
+        jta.setText("");
+    }
+    
+}
