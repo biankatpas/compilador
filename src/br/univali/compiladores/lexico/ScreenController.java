c2202779b4860a4e5499b1b6bf09bb2b56b68517
Bianka Passos <biankatpas@gmail.com>
2018-03-20 21:25:45 -0300

    .gitignore

--- src/br/univali/compiladores/lexico/ScreenController.java
+++ src/br/univali/compiladores/lexico/ScreenController.java
@@ -275,9 +275,6 @@ public class ScreenController
                         jtaMessage.setText("");
                         jf.setTitle("Compilador - " + newFileName);
                     }
-                    else{
-                        newFileName = fileName;
-                    }
                 } else
                 {
                     newFileName = JOptionPane.showInputDialog("Nome do arquivo:");
